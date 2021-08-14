1e990fc6e281be5a17c34859aa88042e39045022
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-08-14 22:44:27 +0500

    update new chagne

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -171,12 +171,12 @@
                         </div>
                         <div class="user-profile-card wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1500ms">
                             <ul>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Donald L. Juarez"><a href="#"><img src="{{asset('assets/img/hero/user-1.jpg')}}" alt="user one"></a></li>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Evan A. Ethridge"><a href="#"><img src="{{asset('assets/img/hero/user-2.jpg')}}" alt="user two"></a></li>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Marcus L. Duncan"><a href="#"><img src="{{asset('assets/img/hero/user-3.jpg')}}" alt="user three"></a></li>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Donald T. Lewis"><a href="#"><img src="{{asset('assets/img/hero/user-4.jpg')}}" alt="user four"></a></li>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Marcus L. Duncan"><a href="#"><img src="{{asset('assets/img/hero/user-5.jpg')}}" alt="user five"></a></li>
-                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Evan A. Ethridge"><a href="#"><img src="{{asset('assets/img/hero/user-6.jpg')}}" alt="user six"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Donald L. Juarez"><a href="#"><img src="{{asset('assets/img/hero/amazon_ico.png')}}" alt="amazon"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Evan A. Ethridge"><a href="#"><img src="{{asset('assets/img/hero/ebay_icon.png')}}" alt="ebay"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Marcus L. Duncan"><a href="#"><img src="{{asset('assets/img/hero/shopify.png')}}" alt="shopify"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Donald T. Lewis"><a href="#"><img src="{{asset('assets/img/hero/bigcommerce.png')}}" alt="bigcommerce"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Marcus L. Duncan"><a href="#"><img src="{{asset('assets/img/hero/wish-logo.png')}}" alt="wish-logo"></a></li>
+                                <li class="user-profile" data-toggle="tooltip" data-placement="top" title="Evan A. Ethridge"><a href="#"><img src="{{asset('assets/img/hero/WooCommerce_logo.png')}}" alt="WooCommerce_logo "></a></li>
                                 <li class="user-profile sign-up" data-toggle="tooltip" data-placement="top" title="Join Now"><a href="#"><i class="fas fa-plus"></i></a></li>
                             </ul>
                         </div>
@@ -813,6 +813,48 @@
             </div> <!-- /.row -->
         </div> <!-- /.container -->
     </section> <!-- /.testimonial-area -->
+      <!--======= Start Brands Slider =======-->
+      <div class="brands-area bg-light-magnolia pt-50 pb-50 wow fadeInUp" data-wow-delay="0ms" data-wow-duration="2500ms">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="brands-slider" id="brands-slider-v2">
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-6.png" alt="brand one">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-7.png" alt="brand two">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-8.png" alt="brand three">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-9.png" alt="brand four">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-10.png" alt="brand five">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-11.png" alt="brand six">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-8.png" alt="brand seven">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-7.png" alt="brand eight">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-9.png" alt="brand nine">
+                        </div>
+                        <div class="brands-item">
+                            <img src="assets/img/brand/brand-11.png" alt="brand ten">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+    </div> <!-- /.brands-area -->
+    <!--======= End Brands Slider =======-->
       <!--====== Start Contact Area ======-->
       <section class="contact-area pt-120 pb-130">
         <div class="container">
@@ -1007,6 +1049,7 @@
             </div> <!-- /.container -->
         </section>  <!-- /.cta-area -->
     <!--====== End Collaboration Area ======-->
+
 
     <!--====== Start Blog & News Area ======-->
    <!-- /.blog-and-news-area -->
